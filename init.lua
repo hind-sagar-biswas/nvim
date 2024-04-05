b603cf83fa85bca7b27041ac347adde73e00bbbb
Hind Sagar Biswas <hindsbhk@gmail.com>
2024-04-05 13:40:15 +0600

    changed theme

--- init.lua
+++ init.lua
@@ -16,5 +16,5 @@ require("remap")
 require("lazy").setup("plugins")
 
 vim.cmd("colorscheme catppuccin")
-vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
